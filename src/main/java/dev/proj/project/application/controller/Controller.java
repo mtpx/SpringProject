c3e6a22b9ea24f53250d7f20bef0abf386bcba0e
mtpx <mateuszkrasny@gmail.com>
2019-12-22 21:11:11 +0100

    register&users_views

--- src/main/java/dev/proj/project/application/controller/Controller.java
+++ src/main/java/dev/proj/project/application/controller/Controller.java
@@ -6,12 +6,13 @@ import dev.proj.project.application.model.User;
 import dev.proj.project.application.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class Controller {
 
     @Autowired
@@ -24,43 +25,48 @@ public class Controller {
         this.userDAO = userDAO;
     }
 
-    @GetMapping(value = "/")
-    public String hello() {
-        return "Hello world";
+    @GetMapping(value = "/userList")
+    public String userList() {
+        return "userList";
+    }
+
+    @GetMapping(value = "/register")
+    public String register() {
+        return "register";
     }
 
     @GetMapping(value = "/users")
-    public Iterable<User> getUsers() {
+    public @ResponseBody Iterable<User> getUsers() {
         return userDAO.findAll();
     }
 
     @GetMapping(value = "/users/{userId}")
-    public Optional<User> getUserById(@PathVariable int userId) {
+    public @ResponseBody Optional<User> getUserById(@PathVariable int userId) {
         return userDAO.findById(userId);
     }
 
     @GetMapping(value = "/usersQuery")
-    public List<User> getUsersQuery(){
+    public @ResponseBody List<User> getUsersQuery(){
         return userService.findAllQuery();
     }
 
     @GetMapping(value = "/usersQuery/{userId}")
-    public User getUserQuery(@PathVariable int userId){
+    public @ResponseBody User getUserQuery(@PathVariable int userId){
         return userService.findByIdQuery(userId);
     }
 
     @PostMapping(value = "/users", consumes = APPLICATION_JSON_VALUE)
-    public void addUser(@RequestBody User user){
+    public @ResponseBody void addUser(@RequestBody User user){
         userDAO.save(user);
     }
 
     @DeleteMapping(value = "/users/{userId}")
-    public void deleteUser(@PathVariable int userId){
+    public @ResponseBody void deleteUser(@PathVariable int userId){
         userDAO.deleteById(userId);
     }
 
     @DeleteMapping(value = "/users")
-    public void deleteAllUsers(){
+    public @ResponseBody void deleteAllUsers(){
         userDAO.deleteAll();
     }
 }
---
+++ src/main/webapp/view/jsp/userList.jsp
@@ -0,0 +1,46 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>PageTitle</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+<button class="button test-ajax" type="button">Pobierz listę użytkowników</button>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">id</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Mail</th>
+    </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+<script type="text/javascript">
+    const apiUrl = "http://localhost:8080/";
+    const $list = $('.table tbody');
+
+    $('.test-ajax').on('click', function () {
+        const $btn = $(this);
+
+        $.ajax({
+            url: apiUrl + '/users',
+            dataType: 'json'
+        })
+            .done((res) => {
+                $list.empty();
+                $.each(res, function (i, item) {
+                    $list.append('<tr><th scope="row">' + res[i].id + '</th><td>' + res[i].firstname + '</td><td>' + res[i].lastname + '</td>' +
+                        '<td>' + res[i].email + '</td></tr>');
+                })
+            })
+            .always(() => {
+            });
+    });
+</script>
+</body>
+</html>
